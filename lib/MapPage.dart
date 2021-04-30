507ffb464b478f513b5cb5a96a8961f7f92ebd79
Chayapon Chantrasagul <chayapon.cha@student.mahidol.ac.th>
2021-04-30 19:14:08 +0700

    Included google maps api and created MapPage class

---
+++ lib/MapPage.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:app_project/DrawerPage.dart';
+import 'package:app_project/HomePage.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> with AutomaticKeepAliveClientMixin {
+  Completer<GoogleMapController> _controller = Completer();
+
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal.shade300,
+        leading: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pop();
+        },
+        child: Icon(
+          Icons.arrow_back
+        ),
+      )),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(13.793406, 100.322514),
+          zoom: 15,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
+  }
+
+  
+}
